0735a82727a3fa476fbcff14c756970da04d9dde
GabrielJesus0 <gabriel053jesus@gmail.com>
2026-05-09 17:20:33 -0300

    Dockerfile UPDATE

--- back-end/src/main/java/com/gerenciador_de_tarefas/back_end/BackEndApplication.java
+++ back-end/src/main/java/com/gerenciador_de_tarefas/back_end/BackEndApplication.java
@@ -2,16 +2,11 @@ package com.gerenciador_de_tarefas.back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackEndApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(BackEndApplication.class, args);
 	}
 
